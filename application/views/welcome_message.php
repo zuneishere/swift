17c43a3d16770d70754f8d5d02b84c3cfe97995f
Ahmad Al-Fakharany <abohmeed@gmail.com>
2017-09-30 00:14:32 +0000

    Changed the title of the home page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Hello to CodeIgniter</title>
+	<title>Hello to Swift</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery-ui.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/styles.css">
